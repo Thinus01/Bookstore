91a14ec6043cf985183a657341cde3f9dc1617e4
Thinus <thinusvdv2001@gmail.com>
2023-02-09 15:36:56 +0200

    Add API storage

--- src/components/bookList.js
+++ src/components/bookList.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import NewBook from './newBook';
 import Book from './book';
+import { fetchHandler } from '../redux/books/books';
 
 const BookList = () => {
-  const booksData = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const bookData = useSelector((state) => state.books);
+
+  useEffect(() => {
+    dispatch(fetchHandler());
+  }, [dispatch]);
+
   return (
     <section>
       <NewBook />
       <div>
         <p>Book</p>
         <p>Author</p>
+        <p>Category</p>
         <ul>
-          {booksData.books.map((book) => (
-            <Book title={book.title} id={book.id} author={book.author} key={book.id} />
+          {bookData.map((book) => (
+            <Book
+              title={book.title}
+              author={book.author}
+              key={book.item_id}
+              category={book.category}
+              id={book.item_id}
+            />
           ))}
         </ul>
       </div>
